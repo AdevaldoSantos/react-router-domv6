b06597dda938b4927548176cea1a0af73d240795
Adevaldo Santos <adevaldosantosfilho@gmail.com>
2024-09-05 18:26:25 -0300

    colocando o projeto para o github

---
+++ src/components/Post/index.tsx
@@ -0,0 +1,10 @@
+import './style.css';
+
+export const Post = () => {
+    return (
+        <div>
+            <h1>Post</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae, ducimus expedita eos voluptatibus, quis fugiat quas recusandae, consectetur iste tempore magnam quidem veniam obcaecati? Impedit velit aperiam ullam quod autem.</p>
+        </div>
+    );
+};
